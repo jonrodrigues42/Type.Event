f082be339885014b8edbbd29d51688d1e37dc141
Jon Rodrigues <joao.nrr@gmail.com>
2023-04-11 00:12:48 -0300

    cadastro de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        return HttpResponse("Testando")
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            return redirect('/usuarios/cadastro')
+        # TODO: Validar força da senha
+
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+
+        return HttpResponse('Ok')
