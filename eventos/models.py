d8c8a77a33fec5097575e6caa01b8750c1760058
Jon Rodrigues <joao.nrr@gmail.com>
2023-04-11 21:31:42 -0300

    create db for evento

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    data_inicio = models.DateField()
+    data_termino = models.DateField()
+    carga_horaria = models.IntegerField()
+    logo = models.ImageField(upload_to="logos")
+    
+    # paleta de cores
+    cor_principal = models.CharField(max_length=7)
+    cor_secundaria = models.CharField(max_length=7)
+    cor_fundo = models.CharField(max_length=7)
