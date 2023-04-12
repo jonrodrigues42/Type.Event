71866c850be312bb3206d78054e1ab01d134d4c6
Jon Rodrigues <joao.nrr@gmail.com>
2023-04-11 21:41:54 -0300

    criado método mágico no nome do evento

--- eventos/models.py
+++ eventos/models.py
@@ -13,3 +13,6 @@ class Evento(models.Model):
     cor_principal = models.CharField(max_length=7)
     cor_secundaria = models.CharField(max_length=7)
     cor_fundo = models.CharField(max_length=7)
+
+    def __str__(self) -> str:
+        return self.nome
