087a6ed0521d9281fabf43178f986c3e3d63faec
Jon Rodrigues <joao.nrr@gmail.com>
2023-04-12 21:45:31 -0300

    criado 'gerenciar_evento' (views e html)

--- eventos/views.py
+++ eventos/views.py
@@ -39,3 +39,9 @@ def novo_evento(request):
         
         messages.add_message(request, constants.SUCCESS, 'Evento cadastrado com sucesso')
         return redirect(reverse('novo_evento'))
+    
+
+def gerenciar_evento(request):
+    if request.method == "GET":
+        eventos = Evento.objects.filter(criador=request.user)
+        return render(request, 'gerenciar_evento.html', {'eventos': eventos})
