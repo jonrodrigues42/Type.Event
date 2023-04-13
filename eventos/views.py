b2081a96f6797596d1b26ab030d1523338c07718
Jon Rodrigues <joao.nrr@gmail.com>
2023-04-12 22:08:48 -0300

    funcionalidade de busca

--- eventos/views.py
+++ eventos/views.py
@@ -43,5 +43,10 @@ def novo_evento(request):
 
 def gerenciar_evento(request):
     if request.method == "GET":
+        nome = request.GET.get('nome')
         eventos = Evento.objects.filter(criador=request.user)
+
+        if nome:
+            eventos = eventos.filter(nome__contains=nome)
+
         return render(request, 'gerenciar_evento.html', {'eventos': eventos})
